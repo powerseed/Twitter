73e04f7816ea1aac53009aacf6a5ef8c54083b03
powerseed <powerseed5044@gmail.com>
2019-12-26 08:22:46 +0000

    initialize style

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,9 +5,26 @@
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   <title>@yield("title", "Twitter")</title>
 </head>
 <body>
-  @yield("content")
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="">Twitter</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item">
+          <a class="nav-link" href="">Help</a>
+        </li>
+        <li>
+          <a class="nav-link" href="">Sign up</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    @yield("content")
+  </div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,9 @@
 @extends("layouts.default")
 @section("title", "Home")
 @section("content")
-  <h1>Home</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">This is Laravel tutorial</p>
+    <div class="btn btn-lg btn-success">Sign up</div>
+  </div>
 @stop
