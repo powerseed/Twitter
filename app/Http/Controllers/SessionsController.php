9b9619286fb856d88f88a16bcde1266d07e1d397
powerseed <powerseed5044@gmail.com>
2019-12-28 04:16:08 +0000

    log in

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view("sessions.create");
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+                'email' => 'required|email|max:255',
+                'password' => 'required'
+            ]
+        );
+
+        if(Auth::attempt($credentials))
+        {
+            session()->flash("success", "Logged in successfully! ");
+            return redirect()->route('users.show', [Auth::user()]);
+        }
+        else
+        {
+            session()->flash("danger", "Invalid username or password. ");
+            return redirect()->back()->withInput();
+        }
+    }
+
+    public function destroy()
+    {
+
+    }
+}
---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,34 @@
+@extends("layouts.default")
+@section("title", "Sign up")
+@section("content")
+  <div class="col-md-8 offset-2">
+    <div class="card">
+      <div class="card-header">
+        <h5>Login</h5>
+      </div>
+      <div class="card-body">
+        @include("shared._messages")
+        @include("shared._error")
+        <form method="post" action="{{ route("login") }}">
+          {{ csrf_field() }}
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input name="email" type="email" class="form-control" value="{{ old("email") }}">
+
+            <label for="password">Password</label>
+            <input name="password" type="password" class="form-control" value="{{ old("password") }}">
+          </div>
+
+          <button class="btn btn-primary">Login</button>
+        </form>
+
+        <hr>
+
+        <p>
+          Don't have an account?
+          <a href="{{ route("signup") }}">Sign up now!</a>
+        </p>
+      </div>
+    </div>
+  </div>
+@stop
